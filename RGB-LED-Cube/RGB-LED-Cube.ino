a063f4e797de314980ea6753edf443879ae68787
maulie5 <83541036+maulie5@users.noreply.github.com>
2023-01-17 23:20:05 +0100

    W.I.P. 0.4

--- RGB-LED-Cube/RGB-LED-Cube.ino
+++ RGB-LED-Cube/RGB-LED-Cube.ino
@@ -3,6 +3,7 @@
 #define LED_PIN D6
 #define LEN_SIDE 3
 CRGB LED_ARRAY[NUM_LEDS];
+int LAYER = LEN_SIDE*LEN_SIDE;
 class LED {
   public:
     LED();
@@ -25,42 +26,56 @@ void createCoord(){
   for(int POSITION = 0; POSITION < NUM_LEDS; POSITION++){
     LEDS_ARRAY[POSITION] = LED(POSITION);
   }
-  for(int LED = 1; LED <= LEN_SIDE; LED++){
-    for(int k = 1; k <= LEN_SIDE; k++){
-      if((k-1)*pow(LEN_SIDE,2) <= LED && LED <= k*pow(LEN_SIDE,2)){
-        LEDS_ARRAY[LED-1].Z_VALUE = (k-1);
+  for(int LED = 0; LED < NUM_LEDS; LED++){
+    for(int z = 1; z <= LEN_SIDE; z++){
+      if( (z-1)*LAYER <= LED && LED <= z*LAYER ){
+        LEDS_ARRAY[LED].Z_VALUE = z-1;
       }
-      if(((k-1)*pow(LEN_SIDE,2) <= LED) && (LEDS <= k*pow(LEN_SIDE,2)) && (k%2 == 1)){
-        for(int j = 1; j <= LEN_SIDE; j++){
-          if(((j-1)*LEN_SIDE+(k-1)*pow(LEN_SIDE,2) < LED) && (LED <= j*LEN_SIDE+k*pow(LEN_SIDE,2))){
-            LEDS_ARRAY[LED-1].Y_VALUE = j-1;
-          }
-          if(j%2 == 1){
-            if(LED%LEN_SIDE == 0){
-              LEDS_ARRAY[LED-1].X_VALUE = LEN_SIDE-1;
-            }else{
-              LEDS_ARRAY[LED-1].X_VALUE = LED%LEN_SIDE-1;
-            }
-          }else{
-            if(LED%LEN_SIDE == 0){
-              LEDS_ARRAY[LED-1].X_VALUE = 0; 
-            }else{
-              LEDS_ARRAY[LED-1].X_VALUE = LED%LEN_SIDE;
-            }
+    }
+    if((LEDS_ARRAY[LED].Z_VALUE%2)==0){
+      for(int LAY = 0; LAY<LEN_SIDE;LAY+=2){
+        for(int y = 1;y <= LEN_SIDE;y++){
+          if(LAY*LAYER+(y-1)*LEN_SIDE <= LED && LED < LAY*LAYER+y*LEN_SIDE){
+            LEDS_ARRAY[LED].Y_VALUE = y-1;
           }
         }
-      }else if((k%2) == 1){
-        for(int l = 1; l <= LEN_SIDE; l++){
-          if(((k-1)*pow(LEN_SIDE,2)+(l-1)*LEN_SIDE < LED) && (LED <= ((k-1)*pow(LEN_SIDE,2)+l*LEN_SIDE)){
-            LEDS_ARRAY[LED-1].Y_VALUE = LEN_SIDE-l;
-          }
-          if(l%2 == 1){
-            if()
+      }
 
-          }
+      if(LEDS_ARRAY[LED].Y_VALUE%2==0){
+        if(LED%LEN_SIDE+1==0){
+          LEDS_ARRAY[LED].X_VALUE = LEN_SIDE ;
+        }else{
+          LEDS_ARRAY[LED].X_VALUE = LED%LEN_SIDE;
+        }
+      }else if(LEDS_ARRAY[LED].Y_VALUE%2==1){
+        if(LED%LEN_SIDE+1==0){
+          LEDS_ARRAY[LED].X_VALUE = 0;
+        }else{
+          LEDS_ARRAY[LED].X_VALUE = LEN_SIDE-(LED%LEN_SIDE)-1;
+        }
+      }
+    }else{
+      int LAYER_POSITION = LED%LAYER;
+      for(int LAY = 0; LAY < LEN_SIDE;LAY++){
+        if(LAYER_POSITION <= (LAY+1)*LEN_SIDE && LAYER_POSITION >= LAY*LEN_SIDE){
+          LEDS_ARRAY[LED].Y_VALUE = LEN_SIDE - LAY-1;
+        }
+      }
+      if(LEDS_ARRAY[LED].Y_VALUE%2==0){
+        if(LED%LEN_SIDE==0){
+          LEDS_ARRAY[LED].X_VALUE = LEN_SIDE-1;
+        }else{
+          LEDS_ARRAY[LED].X_VALUE = (LEN_SIDE-LED%LEN_SIDE)-1;
+        }
+      }else if(LEDS_ARRAY[LED].Y_VALUE%2==1){
+        if(LED%LEN_SIDE==0){
+          LEDS_ARRAY[LED].X_VALUE = 0;
+        }else{
+          LEDS_ARRAY[LED].X_VALUE = LED%LEN_SIDE;
         }
       }
     }
+
   }
 }
 
@@ -70,5 +85,11 @@ void setup() {
 }
 
 void loop() {
-
+  for(int k = 0;k < NUM_LEDS;k++){
+    if(LEDS_ARRAY[k].X_VALUE == 0 || LEDS_ARRAY[k].X_VALUE == 2){
+      LED_ARRAY[k] = CRGB(100,100,100);
+    }else{
+      LED_ARRAY[k] = CRGB(0,0,0);
+    }
+  }
 }
